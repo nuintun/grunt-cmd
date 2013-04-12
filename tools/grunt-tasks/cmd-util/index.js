7683a49827b519f12a6ded1e24d8295702eb8792
NewtonNiu <yongmiui@gmail.com>
2013-04-12 23:15:47 +0800

    Update index.js

--- tools/grunt-tasks/cmd-util/index.js
+++ tools/grunt-tasks/cmd-util/index.js
@@ -1,9 +1,5 @@
 /**
- * Created with JetBrains WebStorm.
- * User: Administrator
- * Date: 13-4-7
- * Time: 上午11:51
- * To change this template use File | Settings | File Templates.
+ * export cmd-util
  */
 exports.ast = require('./lib/ast');
 exports.css = require('./lib/css');
