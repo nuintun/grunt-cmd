c0e7a6d41fb3ae03525a203bc761fadc38b182f0
NewtonNiu <yongmiui@gmail.com>
2013-04-16 10:10:13 +0800

    Update deploy.js

--- tools/grunt-tasks/deploy/deploy.js
+++ tools/grunt-tasks/deploy/deploy.js
@@ -14,7 +14,9 @@ module.exports = function(grunt) {
 
     // normalize uri to linux format
     function normalize(uri) {
-        return path.normalize(uri).replace(/\\/g, '/');
+        var isCurDir = /^\.[/\\]+/.test(uri);
+        uri = path.normalize(uri).replace(/\\/g, '/');
+        return !isCurDir || (isCurDir && uri.charAt(0) === '.') ? uri : './' + uri;
     }
 
     // registerMultiTask
