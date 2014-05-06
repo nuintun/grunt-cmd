5f638987028ab2689bf728ef91ed846901d83fdf
Nuintun <qianduanzu@gmail.com>
2014-05-06 18:16:38 +0800

    update clean

--- tools/grunt-tasks/clean/clean.js
+++ tools/grunt-tasks/clean/clean.js
@@ -1,41 +1,71 @@
 /*
  * grunt-contrib-clean
  * http://gruntjs.com/
- * Copyright (c) 2012 Tim Branyen, contributors
+ *
+ * Copyright (c) 2014 Tim Branyen, contributors
  * Licensed under the MIT license.
  */
 module.exports = function (grunt){
-    var path = require('path');
+    var path = require('path'),
+        rimraf = require('rimraf');
 
     // normalize uri to linux format
     function normalize(uri){
         return path.normalize(uri).replace(/\\/g, '/');
     }
 
-    // register task
+    function clean(filepath, options){
+        filepath = normalize(filepath);
+
+        if (!grunt.file.exists(filepath)) {
+            return false;
+        }
+
+        grunt.log.write('$ '.green
+            + (options['no-write'] ? 'Not actually cleaning ' : 'Cleaning ').cyan
+            + filepath.grey + '...');
+
+        // Only delete cwd or outside cwd if --force enabled. Be careful, people!
+        if (!options.force) {
+            if (grunt.file.isPathCwd(filepath)) {
+                grunt.verbose.error();
+                grunt.fail.warn('Cannot delete the current working directory.'.red);
+                return false;
+            } else if (!grunt.file.isPathInCwd(filepath)) {
+                grunt.verbose.error();
+                grunt.fail.warn('Cannot delete files outside the current working directory.'.red);
+                return false;
+            }
+        }
+
+        try {
+            // Actually delete. Or not.
+            if (!options['no-write']) {
+                rimraf.sync(filepath);
+            }
+
+            grunt.log.ok();
+        } catch (e) {
+            grunt.log.error();
+            grunt.fail.warn('Unable to delete '.red
+                + filepath.grey + (' file (' + e.message + ').').red, e);
+        }
+    }
+
     grunt.registerMultiTask('clean', 'Clean files and folders.', function (){
-        var that = this,
-            options = that.options({
-                force: false
-            });
+        var options, that = this;
+
+        // Merge task-specific and/or target-specific options with these defaults.
+        options = that.options({
+            force: grunt.option('force') === true,
+            'no-write': grunt.option('no-write') === true
+        });
 
         console.time('$'.green + ' Clean time consuming'.cyan);
 
-        grunt.verbose.writeflags(options, 'Options');
         // Clean specified files / dirs.
-        that.filesSrc.forEach(function (filepath){
-            filepath = normalize(filepath);
-
-            grunt.log.write('$ '.green + 'Cleaning '.cyan + filepath.grey + ' ...');
-
-            try {
-                grunt.file.delete(filepath, options);
-                grunt.log.ok();
-            } catch (e) {
-                grunt.log.error();
-                grunt.verbose.error(e);
-                grunt.fail.warn('Clean operation failed'.red);
-            }
+        this.filesSrc.forEach(function (filepath){
+            clean(filepath, options);
         });
 
         console.timeEnd('$'.green + ' Clean time consuming'.cyan);
